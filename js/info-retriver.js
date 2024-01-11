dad19ed109b116ebc4880f54ddad6b4577895177
Malin Kalfjäll <mk225ii@student.lnu.se>
2024-01-11 20:45:36 +0100

    last commit

--- js/info-retriver.js
+++ js/info-retriver.js
@@ -1,20 +1,22 @@
+// Funktionen getData tar en parameter dateToFetch, men den används inte i funktionen.
 
 function getData(dateToFetch) {
-
+  
+  // En API-nyckel för att använda OpenWeatherMap-tjänsten tilldelas till variabeln apiKey.
   const apiKey = "917225c5756b7579b37fc766deff8b4c";
 
-  const url = "https://api.openweathermap.org/data/2.5/forecast?q=Norrkoping,Sweden&units=metric&appid=" + apiKey
+  // En URL sammansätts med staden (Norrköping, Sweden), enheter (metric) och API-nyckel.
+  const url = "https://api.openweathermap.org/data/2.5/forecast?q=Norrkoping,Sweden&units=metric&appid=" + apiKey;
 
-
+  // En HTTP-begäran görs med fetch för att hämta data från den angivna URL:en.
   return fetch(url)
-
+    // Efter att begäran är slutförd, konverteras responsen till JSON-format.
     .then(response => {
-      return response.json()
+      return response.json();
     })
-
+    // Den parsade JSON-datan returneras, specifikt "list" i det här fallet.
     .then(data => data.list);
-
 }
 
-
+// Funktionen exporteras för att kunna användas från andra filer.
 export { getData };
--- js/script.js
+++ js/script.js
@@ -1,68 +1,58 @@
+// Importera funktionen getData från info-retriver.js
 import { getData } from "./info-retriver.js";
 
+// Hämta elementet med klassen "weather-forecast" och lagra det i variabeln msgElem
 let msgElem = document.getElementsByClassName("weather-forecast");
+
+// Anropa getData-funktionen asynkront och lagra resultatet i variabeln dateToFetch
 const dateToFetch = await getData();
+
+// Lägg till en händelselyssnare för "load" -händelsen på fönstret och anropa init-funktionen
 window.addEventListener("load", init());
 
+// Initiera funktionen init
 function init() {
+  // Lägg till en händelselyssnare för "change" -händelsen på elementet med id "datum" och anropa showWeather-funktionen
   document.getElementById("datum").addEventListener("change", showWeather);
 }
 
+// Funktionen showWeather visar väderinformation baserat på det valda datumet
 function showWeather() {
-  let imageUrl; // Declare imageUrl here
+  // Deklarera variabler för bild-URL och väderförhållanden
+  let imageUrl = "";
+  let weatherMain = "";
 
-  for (let i = 0; i < dateToFetch.length; i++) {
-    if (dateToFetch[i].dt_txt.split(" ")[0] === document.getElementById("datum").value) {
-      const weatherMain = dateToFetch[i].weather[0].main;
-
-      // Get the corresponding image number based on weatherMain
-      let imageNumber;
-      switch (weatherMain) {
-        case "Clouds":
-          imageNumber = 1;
-          break;
-        case "Rain":
-          imageNumber = 2;
-          break;
-        case "Snow":
-          imageNumber = 3;
-          break;
-        case "Sun":
-          imageNumber = 4;
-          break;
-        default:
-          imageNumber = 1; // Default to cloudy image if unknown weather condition
-      }
-
-      // Get the image URL using the geticon function
-      imageUrl = geticon(imageNumber);
-
-      // Display the image
-      const imgElement = document.createElement("img");
-      imgElement.src = imageUrl;
-      imgElement.alt = weatherMain;
-      document.getElementById("weather-icons").innerHTML = ''; // Clear existing content
-      document.getElementById("weather-icons").appendChild(imgElement);
+  // Loopa igenom dateToFetch för att hitta väderförhållandet för det valda datumet
+  for (let i = 0; i < dateToFetch.length; i++) { //En for-loop används för att iterera över varje objekt i dateToFetch-arrayen, som förväntas innehålla väderinformation för flera dagar.
+    if (dateToFetch[i].dt_txt.split(" ")[0] === document.getElementById("datum").value) { //Inuti loopen används en if-sats för att kontrollera om datumet från det aktuella objektet i arrayen matchar det datum som användaren har valt på webbsidan. 
+      weatherMain = dateToFetch[i].weather[0].main;
+      break;
     }
   }
-}
 
-function geticon(nr) {
-  let icon;
-  // Använder en switch-sats för att välja rätt bild baserat på fruktnumret
-  switch (nr) {
-    case 1:
-      icon = "../img/cloudy.png";
+  // Använd en switch-sats för att matcha väderförhållandet och tilldela rätt bild-URL
+  switch (weatherMain) {
+    case "Clouds":
+      imageUrl = "../img/cloudy.png";
       break;
-    case 2:
-      icon = "../img/rain.png";
+    case "Rain":
+      imageUrl = "../img/rain.png";
       break;
-    case 3:
-      icon = "../img/snowy.png";
+    case "Snow":
+      imageUrl = "../img/snowy.png";
       break;
-    case 4:
-      icon = "../img/sun.png";
+    case "Sun":
+      imageUrl = "../img/sun.png";
+      break;
+    default:
+      imageUrl = "../img/ingen-data.png";
       break;
   }
-  return icon;
+
+  // Skapa ett bildlement, tilldela bild-URL och alt-attribut, rensa befintligt innehåll och lägg till bilden i DOM
+  const imgElement = document.createElement("img");
+  imgElement.src = imageUrl;
+  imgElement.alt = weatherMain;
+  document.getElementById("weather-icons").innerHTML = ''; // Rensa befintligt innehåll
+  document.getElementById("weather-icons").appendChild(imgElement);
 }
